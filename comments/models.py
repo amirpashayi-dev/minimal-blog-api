84f72befc7c2cd801f1a2ff999d4509717fa500f
amirpashayi-dev <amirpashayi.dev@gmail.com>
2025-04-11 00:22:25 +0330

    implement Comment model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,29 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from accounts.models import User
+from posts.models import Post
 
-# Create your models here.
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_approved = models.BooleanField(default=False)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
+    level = models.PositiveSmallIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    def save(self, *args, **kwargs):
+        if self.parent:
+            self.level = self.parent.level + 1
+        else:
+            self.level = 1
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.content[:30]}'
+
+    class Meta:
+        ordering = ['-created_at']
+
+
