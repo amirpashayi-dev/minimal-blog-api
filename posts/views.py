cac6238420b00c3a36e99b08b16fcf99a4bf9f62
amirpashayi-dev <amirpashayi.dev@gmail.com>
2025-04-12 14:51:53 +0530

    add Pagination for AuthorPosts API

--- posts/views.py
+++ posts/views.py
@@ -5,6 +5,7 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
 from .serializers import PostSerializer, AuthorPostsSerializer
 from .models import Post
 from accounts.models import User
@@ -45,8 +46,12 @@ class AuthorPostsAPIView(APIView):
         posts = Post.objects.filter(user__username=username, status='published')
         for backend in self.filter_backends:
             posts = backend().filter_queryset(request, posts, self)
+
+        paginator = PageNumberPagination()
+        paginated_posts = paginator.paginate_queryset(posts, request, view=self)
+
         data = {
-            'posts': posts,
+            'posts': paginated_posts,
             'author': author
         }
         ser_data = AuthorPostsSerializer(instance=data)
